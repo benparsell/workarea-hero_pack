11101510b18417e713638e49d9415f633c627845
Ben Parsell <parsellb@mail.gvsu.edu>
2021-01-02 15:41:28 -0500

    Initial commit

---
+++ lib/workarea/hero_pack.rb
@@ -0,0 +1,8 @@
+require 'workarea'
+require 'workarea/hero_pack/engine'
+require 'workarea/hero_pack/version'
+
+module Workarea
+  module HeroPack
+  end
+end
---
+++ lib/workarea/hero_pack/version.rb
@@ -0,0 +1,5 @@
+module Workarea
+  module HeroPack
+    VERSION = "1.0.0.pre".freeze
+  end
+end
